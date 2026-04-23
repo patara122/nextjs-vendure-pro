7934a803278254898f3f4d468426228f4b70c070
patara122 <patara.a@gmail.com>
2026-04-23 12:02:45 +0700

    first commit

---
+++ next.config.ts
@@ -0,0 +1,31 @@
+import { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+
+const nextConfig: NextConfig = {
+    cacheComponents: true,
+    images: {
+        // This is necessary to display images from your local Vendure instance
+        dangerouslyAllowLocalIP: true,
+        remotePatterns: [
+            {
+                hostname: 'readonlydemo.vendure.io',
+            },
+            {
+                hostname: 'demo.vendure.io'
+            },
+            {
+                hostname: 'localhost'
+            },
+            {
+                hostname: 's3.hhh.co.th'
+            }
+        ],
+    },
+    experimental: {
+        rootParams: true
+    }
+};
+
+export default withNextIntl(nextConfig);
